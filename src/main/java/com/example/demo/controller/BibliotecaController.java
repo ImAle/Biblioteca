75a896f194258088f43550e7f11c28c00d545ccd
Álvaro <alvarocc948@gmail.com>
2024-11-21 20:26:07 +0100

    Vista Index y errores 404 y 500

--- src/main/java/com/example/demo/controller/BibliotecaController.java
+++ src/main/java/com/example/demo/controller/BibliotecaController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.entity.Usuario;
 
 @Controller
 @RequestMapping("/principal")
@@ -15,4 +18,13 @@ public class BibliotecaController {
 		return "index";
 	}
 	
+	// ====== Para probar el error 500 ======
+	/*
+	@GetMapping("/prueba")
+	public String prueba(Model model) {
+		 model.addAttribute("usuario", new Usuario());
+		 int i = 6/0;
+		 return INDEX_VIEW;
+	 }
+	 */
 }
