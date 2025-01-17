b4fb1a74ad3fb7725069044ab00f2924cd942277
ImAle <alejandrogalldom@gmail.com>
2025-01-17 19:44:10 +0100

    Agregado controlador de prestamos para usuario

--- src/main/java/com/example/demo/controller/PrestamoController.java
+++ src/main/java/com/example/demo/controller/PrestamoController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Libro;
+
 import com.example.demo.entity.Prestamo;
 import com.example.demo.entity.Usuario;
 import com.example.demo.service.LibroService;
@@ -14,10 +15,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -35,6 +37,12 @@ public class PrestamoController {
     @Autowired
     @Qualifier("libroService")
     private LibroService libroService;
+    
+    @GetMapping("")
+    public String verPrestamos(@AuthenticationPrincipal Usuario usuario, Model model) {
+    	model.addAttribute("prestamos", prestamoService.getPrestamosActivosByUserId(usuario.getId()).stream().map(p -> p.getLibro()).toList());
+    	return "/usuario/verPrestamos";
+    }
 
     @PostMapping("/{id}")
     public String prestamoLibro(@PathVariable("id") Long id, @AuthenticationPrincipal Usuario usuario, RedirectAttributes redirect){
